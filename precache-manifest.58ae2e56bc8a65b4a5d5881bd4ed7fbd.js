a2652238b926cb4dc162d9f913893f11ba2994d0
istamal <dagalaev@gmail.com>
2020-06-02 16:59:12 +0300

    Updates

---
+++ precache-manifest.58ae2e56bc8a65b4a5d5881bd4ed7fbd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b8c46fcd3a076f53335aca58db14fc0",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "a6f0c46cb5db78f29568",
+    "url": "/blog/static/css/2.4b6e3ef2.chunk.css"
+  },
+  {
+    "revision": "05d2f7bddbf17b330f41",
+    "url": "/blog/static/css/main.bb503ba2.chunk.css"
+  },
+  {
+    "revision": "a6f0c46cb5db78f29568",
+    "url": "/blog/static/js/2.ad4492f8.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/blog/static/js/2.ad4492f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05d2f7bddbf17b330f41",
+    "url": "/blog/static/js/main.07dc963c.chunk.js"
+  },
+  {
+    "revision": "335f892cbdce700a9777",
+    "url": "/blog/static/js/runtime-main.1a1c4d23.js"
+  }
+]);
